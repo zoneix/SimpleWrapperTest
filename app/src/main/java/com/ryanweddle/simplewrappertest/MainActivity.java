7634036f3e17a143d9dbb3b8fd3ec637b84381cf
zoneix <zoneix@gmail.com>
2018-03-16 14:52:25 -0400

    Added HTML Assets for Demo
    Ozark Quick Connect Refactor

--- app/src/main/java/com/ryanweddle/simplewrappertest/MainActivity.java
+++ app/src/main/java/com/ryanweddle/simplewrappertest/MainActivity.java
@@ -4,12 +4,20 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.webkit.WebView;
+import android.webkit.WebChromeClient;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
+import android.webkit.JavascriptInterface;
+import android.widget.Toast;
+import android.content.Context;
 
 import com.ryanweddle.sparksdkwrapper.SparkCall;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView myWebView;
 
     private Button mCallButton;
     private EditText mCallEdit;
@@ -20,6 +28,20 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+        myWebView = (WebView)findViewById(R.id.splash_webview);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        myWebView.getSettings().setLoadWithOverviewMode(true);
+        myWebView.setWebChromeClient(new WebChromeClient());
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.addJavascriptInterface(new WebAppInterface(this),"Android");
+        myWebView.loadUrl("file:///android_asset/www/index.html");
+
+
+        /*
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -40,7 +62,58 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
 
         });
+        */
 
+    }
 
+    @Override
+    public void onBackPressed() {
+        if(myWebView.canGoBack()) {
+            myWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    //try WebAppInterface code here -
+
+    public class WebAppInterface {
+        Context mContext;
+
+        /**
+         * Instantiate the interface and set the context
+         */
+        WebAppInterface(Context c) {
+            mContext = c;
+        }
+
+        @JavascriptInterface
+        public void StartDemo (String jot, String uri)
+        {
+            //Toast.makeText(mContext,jot, Toast.LENGTH_SHORT).show();
+            Toast.makeText(mContext,"Received JWT from AWS Lambda",Toast.LENGTH_SHORT).show();
+            Log.i(CLASS_TAG, "Call Button Pressed");
+            Log.i(CLASS_TAG, "The context is - "+ mContext);
+            Intent intent = new Intent(mContext, SparkCall.class);
+            intent.putExtra(SparkCall.INTENT_CALLEE, uri);
+            intent.putExtra(SparkCall.INTENT_JWT, jot);
+            Log.i(CLASS_TAG, "The intent is - "+ intent);
+            /*
+            EditText editText = (EditText) findViewById(R.id.dummy_view_text);
+            String newmessage = editText.getText().toString();
+            intent.putExtra(message, newmessage);
+            */
+            startActivity (intent);
+
+        }
+
+        /*
+        @JavascriptInterface
+        public void moveToNextScreen() {
+            Intent intRef = new Intent(LoginActivity.this, SecondActivity.class);
+            startActivity(intRef);
+            //Log.e("banji", "Clicked");
+        }
+        */
     }
 }
